2cad62ee6d85de848080e71a71eada50ddaeb649
Dmitry Andrievsky <dkandrievsky@luxoft.com>
2019-10-25 16:16:15 +0300

    show num of persons in tour list

--- TourCalcWebApp/App/containers/tour-list.jsx
+++ TourCalcWebApp/App/containers/tour-list.jsx
@@ -78,11 +78,12 @@ export default class TourList extends React.Component {
                                             <TableCell>
                                                 {idx + 1}.<Link key={'l' + idx} to={'/tour/' + t.id}>{t.name}
                                                 </Link>&nbsp;
-                                                [{t.persons.filter(p => (p.receivedInCents - p.spentInCents) >= 0).length > 0 ? ((1 -
+                                                [p: <b>{t.persons.length}</b> s: <b>{t.spendings.length}</b> closure:
+                                                <b>{t.persons.filter(p => (p.receivedInCents - p.spentInCents) >= 0).length > 0 ? ((1 -
                                                     t.persons.filter(p => (p.receivedInCents - p.spentInCents) > 0).length * 1.0 /
                                                     t.persons.filter(p => (p.receivedInCents - p.spentInCents) >= 0).length)
                                                     * 100).toFixed(0) : 0
-                                                }%]
+                                                }%</b>]
                                             </TableCell>
                                             <TableCell>
                                                 <Button variant='outlined' onClick={() => { document.getElementById('TourJsonTextArea').value = JSON.stringify(t, null, 2); }}>JSON</Button>
