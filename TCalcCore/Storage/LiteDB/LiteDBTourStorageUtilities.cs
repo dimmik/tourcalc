3b4067ebf492dee050f68c24f401ca2fd3355403
Dmitry A <ad@dimmik.org>
2019-10-28 22:28:03 +0300

    version names

--- TCalcCore/Storage/LiteDB/LiteDBTourStorageUtilities.cs
+++ TCalcCore/Storage/LiteDB/LiteDBTourStorageUtilities.cs
@@ -45,7 +45,7 @@ namespace TCalc.Storage.LiteDB
             using (var db = new LiteDatabase(path))
             {
                 var q = db.GetCollection<Tour>("Tour").Find(predicate)
-                    .Where(t => t.VersionFor_Id == tourId);
+                    .Where(t => t.IsVersion && t.VersionFor_Id == tourId);
                 totalCount = q.Count();
                 var tours = q
                     .Skip(from).Take(count);
@@ -75,11 +75,11 @@ namespace TCalc.Storage.LiteDB
                 hTour.DateVersioned = DateTime.Now;
                 hTour.VersionFor_Id = tour.Id;
                 hTour.VersionComment = tour.InternalVersionComment ?? new Func<string>(() => {
-                    if (hTour.Persons.Count() > tour.Persons.Count) return "Person deleted";
-                    if (hTour.Persons.Count() < tour.Persons.Count) return "Person added";
-                    if (hTour.Spendings.Count() < tour.Spendings.Count) return "Spending added";
-                    if (hTour.Spendings.Count() > tour.Spendings.Count) return "Spending deleted";
-                    return "Text or nums changed";
+                    if (hTour.Persons.Count() > tour.Persons.Count) return $"P '{ hTour.Persons.Except(tour.Persons).Last()?.Name ?? "--" }' deleted";
+                    if (hTour.Persons.Count() < tour.Persons.Count) return $"P '{ tour.Persons.Last()?.Name ?? "--" }' added";
+                    if (hTour.Spendings.Count() < tour.Spendings.Count) return $"S '{tour.Spendings.Last()?.Description ?? "--" }' added";
+                    if (hTour.Spendings.Count() > tour.Spendings.Count) return $"S '{hTour.Spendings.Except(tour.Spendings).Last()?.Description ?? "--" }' deleted";
+                    return "Names or numbers changed";
                     })();
                 //hTour.Name = $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm")} {tour.Name}";
                 UpsertTour(hTour, path);
