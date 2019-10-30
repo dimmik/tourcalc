990ae269c70c0d0822865dbded33d352120c411c
Dmitry Andrievsky <dkandrievsky@luxoft.com>
2019-10-30 18:24:16 +0300

    remove page load timestamp

--- TourCalcWebApp/Controllers/HomeController.cs
+++ TourCalcWebApp/Controllers/HomeController.cs
@@ -80,7 +80,7 @@ namespace TourCalcWebApp.Controllers
     <div id='content'>
     </div>
 <br/>
-<span style='font-size: xx-small'>v_ver_ [_now_]</span>
+<span style='font-size: xx-small'>v_ver_ <!--[_now_]--></span>
     <script type='text/javascript' src='/assets/bundle.js?unic=_md5_' 
         charset=""utf-8""></script>
 </body>
