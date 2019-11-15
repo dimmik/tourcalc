f6b1a8464da26b127eb7925bbfac0f1d38dd32aa
Dmitry Andrievsky <dkandrievsky@luxoft.com>
2019-11-15 13:04:51 +0300

    Refactor: move tourlist to domain

---
+++ TCalcCore/Domain/TourList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TCalc.Domain
+{
+    public class TourList
+    {
+        public IEnumerable<Tour> Tours { get; set; }
+        public int TotalCount { get; set; }
+        public int From { get; set; }
+        public int Count { get; set; }
+        public int RequestedCount { get; set; }
+    }
+
+}
