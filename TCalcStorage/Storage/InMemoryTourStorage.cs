a73402c8178d6e6f7c4dacb35b24e6db1f348883
dkandrievsky@luxoft.com <dkandrievsky@luxoft.com>
2021-08-26 15:45:27 +0300

    in memory tour storage

---
+++ TCalcStorage/Storage/InMemoryTourStorage.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using TCalc.Domain;
+using TCalc.Storage;
+
+namespace TCalcStorage.Storage
+{
+    public class InMemoryTourStorage : ITourStorage
+    {
+        private readonly List<Tour> Tours;
+
+        public InMemoryTourStorage(List<Tour> tours = null)
+        {
+            Tours = tours ?? new List<Tour>();
+        }
+
+        public void AddTour(Tour tour)
+        {
+            Tours.Add(tour);
+        }
+
+        public void DeleteTour(string tourid)
+        {
+            Tours.RemoveAll(t => t.Id == tourid);
+        }
+
+        public Tour GetTour(string tourid)
+        {
+            return Tours.FirstOrDefault(t => t.Id == tourid);
+        }
+
+        public IEnumerable<Tour> GetTours(Expression<Func<Tour, bool>> predicate, bool includeVersions, int from, int count, out int totalCount)
+        {
+            var coll = Tours;
+            Expression<Func<Tour, bool>> isVersion;
+            if (includeVersions)
+            {
+                isVersion = t => true;
+            }
+            else
+            {
+                isVersion = t => !t.IsVersion;
+            }
+            var allTours = coll
+                .AsQueryable()
+                .Where(isVersion)
+                .Where(predicate)
+                .OrderByDescending(t => t.DateCreated);
+            totalCount = (int)allTours.Count();
+            var tours = allTours.Skip(from).Take(count).ToList();
+            return tours;
+        }
+
+        public IEnumerable<Tour> GetTourVersions(Expression<Func<Tour, bool>> predicate, string tourId, int from, int count, out int totalCount)
+        {
+            var coll = Tours;
+            Expression<Func<Tour, bool>> vpr = (t) => (t.IsVersion && t.VersionFor_Id == tourId);
+
+            var allVersions = coll.AsQueryable<Tour>()
+                .Where(predicate)
+                .Where(vpr)
+                .OrderByDescending(t => t.DateVersioned);
+
+            totalCount = (int)allVersions.Count();
+            var versions = allVersions.Skip(from).Take(count).ToList();
+            return versions;
+        }
+
+        public void StoreTour(Tour tour)
+        {
+            int idx = Tours.FindIndex(0, Tours.Count, t => t.Id == tour.Id);
+            if (idx == -1)
+            {
+                AddTour(tour);
+                return;
+            }
+            Tours[idx] = tour;
+        }
+    }
+}
