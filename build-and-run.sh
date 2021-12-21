ab6cf99fef20bc092b7906933e7d2819c481ff23
ad@dimmik.org <ad@dimmik.org>
2021-12-21 11:08:09 +0300

    remove container before start in script

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 podman build -f tourcalc.run.docker -t tourcalc:latest --no-cache .
 podman stop tourcalc
+podman rm tourcalc
 podman run -d -p 127.0.0.1:8080:80 --env-file ~/config/tourcalc.env --name tourcalc localhost/tourcalc:latest 
