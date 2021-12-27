2346780db5565b5c3816f3a82466b2195fd5a05a
ad@dimmik.org <ad@dimmik.org>
2021-12-27 13:25:57 +0300

    hmmm

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ changed=0;
 git pull | grep "Already up" || changed=1
 #echo "ch: $changed"
 if [ $changed -eq 1 ]; then
-  echo "Rebuilding"
+  echo "Rebuilding!"
   podman build -f tourcalc.run.docker -t tourcalc:latest --no-cache .
 fi
 
 if [ $changed -eq 0 ]; then
-  echo "NO news. Do not rebuild"
+  echo "NO news. Do not rebuild!"
 fi
 
