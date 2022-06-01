5e3f79fc2edeaaf06a6d0e0d12cb50da409bbf8b
Dmitry Andrievsky <ad@dimmik.org>
2022-06-02 00:34:08 +0300

    slight refactoring

---
+++ TCBlazor/Client/Utils/HttpExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+
+namespace TCBlazor.Client.Utils
+{
+    public static class HttpExtensions
+    {
+        public async static Task<T> GetFromJsonWithAuthToken<T>(this HttpClient http, string url, string token)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, url);
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", token);
+            var resp = await http.SendAsync(request);
+            T? t = default;
+            if (resp.IsSuccessStatusCode)
+            {
+                t = await resp.Content.ReadFromJsonAsync<T>();
+            }
+            return t;
+        }
+    }
+}
