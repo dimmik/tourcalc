f61ebde6da25f4e2f34aded87b08f45b89b919f2
dimmik <ad@dimmik.org>
2022-06-17 12:23:58 +0300

    default redirector tour page is add spending

--- TCBlazor/Client/Storage/UISettings.cs
+++ TCBlazor/Client/Storage/UISettings.cs
@@ -6,5 +6,6 @@
         /*public DateTimeOffset SomeDate { get; set; } = DateTimeOffset.Now;
         public string SomeString { get; set; } = "aaa";
         public string NotAProp = "";*/
+        public bool Default_Tour_Page_Is_Add_Spending { get; set; } = true;
     }
 }
