66a21c72ab0b5928fec99a3a5063a324d5a829c1
dimmik <ad@dimmik.org>
2022-07-05 14:25:03 +0300

    pie color scheme adjustable

--- TCalcCore/UI/UISettings.cs
+++ TCalcCore/UI/UISettings.cs
@@ -3,9 +3,11 @@
     public class UISettings
     {
         public int MinimumMeaningfulDebt { get; set; } = 49;
+        public int Magic_Piechart_Color_Scheme_Number { get; set; } = 1630;
         /*public DateTimeOffset SomeDate { get; set; } = DateTimeOffset.Now;
         public string SomeString { get; set; } = "aaa";
         public string NotAProp = "";*/
         public bool Default_Tour_Page_Is_Add_Spending { get; set; } = true;
+        public bool Show_Debug_UI { get; set; } = false;
     }
 }
