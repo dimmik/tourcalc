b7b2e195776bf23eb1048507fa250693e5bb2507
Dmitry Andrievsky <ad@dimmik.org>
2022-07-10 22:10:32 +0300

    good-looking number representation (like 123 456 78) everywhere (hope so)

---
+++ TCalcCore/UI/GlobConsts.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace TCalcCore.UI
+{
+    public static class GlobConsts
+    {
+        public static NumberFormatInfo NumGroupSpaceSeparated = new NumberFormatInfo() { NumberGroupSeparator = " " };
+    }
+}
