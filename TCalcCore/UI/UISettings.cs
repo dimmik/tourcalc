f523d7b1f8cf9d3cc3462770380ba7fd4b5191e2
dimmik <ad@dimmik.org>
2022-12-15 12:58:29 +0100

    #262 configurable default ToAll  for spendings

--- TCalcCore/UI/UISettings.cs
+++ TCalcCore/UI/UISettings.cs
@@ -10,5 +10,6 @@
         public bool Collapse_Columns_In_Person_List_On_Smaller_Screen { get; set; } = true;
         public int Smaller_Screen_Width { get; set; } = 500;
         public bool In_Add_Spending_Page_Filter_by_Chosen_Payer { get; set; } = false;
+        public bool Spending_ToAll_DefaultOn { get; set; } = true;
     }
 }
