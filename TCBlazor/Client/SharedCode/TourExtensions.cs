f15b8877f8e727730416d58e4a791a85812ade6e
dimmik <ad@dimmik.org>
2024-03-01 14:41:53 +0100

    min in correct currency

---
+++ TCBlazor/Client/SharedCode/TourExtensions.cs
@@ -0,0 +1,19 @@
+﻿using AntDesign;
+using TCalc.Domain;
+
+namespace TCBlazor.Client.SharedCode
+{
+    public static class TourExtensions
+    {
+        public static int GetAmountInCurrentCurrencyFromMinValued(this Tour tour, int amount)
+        {
+            if (tour == null) return amount;
+            if (!tour.IsMultiCurrency()) return amount;
+            var currentCurrencyRate = tour.Currency.CurrencyRate;
+            var minCurrencyRate = tour.Currencies.Min(c => c.CurrencyRate);
+            var rate = minCurrencyRate*1.0/currentCurrencyRate;
+            int result = (int)Math.Floor(amount * rate);
+            return result;
+        }
+    }
+}
