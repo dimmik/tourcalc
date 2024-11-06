b77e60ac5b80da670eb1e0a0b69dcf560a8010fb
Dmitry Andrievskiy <dmitry.andrievsky@dxc.com>
2024-11-06 09:43:36 +0100

    should be implemented another way

--- TCalcCore/Domain/Spending.cs
+++ TCalcCore/Domain/Spending.cs
@@ -16,7 +16,9 @@ namespace TCalc.Domain
         public string Description { get; set; } = "";
         public string Type { get; set; } = "Common";
         public Currency Currency { get; set; } = new Currency();
-        public IEnumerable<Currency> Currencies { get; set; } = Enumerable.Empty<Currency>();
+        // 2024-11-06 do not keep/track currency rates
+        //public IEnumerable<Currency> Currencies { get; set; } = Enumerable.Empty<Currency>();
+        public IEnumerable<Currency> Currencies => Enumerable.Empty<Currency>();
         public bool IsDryRun { get; set; } = false;
         public bool IncludeDryRunInCalc { get; set; } = false;
         public string Color { get; set; } = "";
