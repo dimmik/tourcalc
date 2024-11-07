8f582f7d2b2e0ae54dcc179c01a54387e5e76aac
Dmitry Andrievskiy <dmitry.andrievsky@dxc.com>
2024-11-07 10:05:28 +0100

    generalize cloning through json

---
+++ TCalcCore/Helpers/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using TCalc.Domain;
+using TCalcCore.Domain;
+
+namespace TCalcCore.Helpers
+{
+    internal static class ExtensionMethods
+    {
+        public static T Clone<T>(this T item) where T : ITourcalcItem
+        {
+            var json = JsonConvert.SerializeObject(item);
+            var clone = JsonConvert.DeserializeObject<T>(json);
+            return clone;
+        }
+    }
+}
